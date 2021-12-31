c3b22fe5f5679baade29ea1e60487219768f3eca
fati <etsukazu200@gmail.com>
2021-12-31 17:27:07 +0100

    creation des classes

--- src/etrainement.js
+++ src/etrainement.js
@@ -1,7 +1,24 @@
 // 1. declare une class Livre .  Avec son constructeur, initialize 2 attributs ,  titre , auteur
 // 2. cree une fonction show qui affiche le titre et nom d'auteur
 
+class Livre{
+ titre;
+auteur;
+constructor(titre,auteur){
+this.titre=titre;
+this.auteur=auteur;
 
+}
+
+
+    Show(){
+
+console.log(this.titre+" "+this.auteur);
+ console.log(Livre.titre+" "+Livre.auteur);
+
+} 
+
+}
 
 
 
@@ -10,7 +27,7 @@
 //3. cree une instance de de type Livre , avec les argument titre = les mesirable , auteur ="victor Higo"
 //4. faire appel à la fonction  show
 
-
+var book=new Livre("les mesirable","victor Higo");
 
 
 //5. cree une autre instance de type Livre
