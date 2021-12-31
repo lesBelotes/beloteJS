8cce73552964a5b45ee482000703dbde19604290
abdessamad-boulzergue <abdosblz@gmail.com>
2021-12-31 13:52:23 +0000

    entrainement

---
+++ src/etrainement.js
@@ -0,0 +1,25 @@
+// crée une tableau vide  avec nom produits en ligne 3
+
+
+
+// crée un un object prod1 et prod2 : qui contient 3 attributs  id , nom , prix
+
+
+
+// ajouter les des produits (prod1, prod2) dans la table produits 
+
+
+
+// cree une fonction qui prend en paramettre un tableau produit et affiche les noms et prix  de des produits
+
+
+
+
+
+
+// faire appel à la fonction qui est precedement crée 
+
+
+
+// crée une fonction qui prends en paramettre un object produit et qui renvoie le meme objet apres avoir multiplié son prix par deux
+// faire appel à cette fonction et affiche le resultat
