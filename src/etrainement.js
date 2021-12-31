5ae090b136df961d0775c582ad5d60e86e577e2d
fati <etsukazu200@gmail.com>
2021-12-31 16:04:21 +0100

    reponse

--- src/etrainement.js
+++ src/etrainement.js
@@ -1,25 +1,39 @@
 // crée une tableau vide  avec nom produits en ligne 3
 
-
+let produits=[];
+let produitsenligne3=new Array();
 
 // crée un un object prod1 et prod2 : qui contient 3 attributs  id , nom , prix
-
+let prod1={id:1,nom:'créme main',prix:'145dh'};
+let prod2={id:2,nom:'créme pied',prix:'90dh'};
 
 
 // ajouter les des produits (prod1, prod2) dans la table produits 
-
-
+    
+  produits=[prod1,prod1];
 
 // cree une fonction qui prend en paramettre un tableau produit et affiche les noms et prix  de des produits
 
+let afficheproduct=function(produits){
+  for(var i=0;i<produits.length;i++)
+     console.log("nom du produit "+produits[i].nom+""+"prix"+produits[i].prix);
 
+}
 
 
 
 
 // faire appel à la fonction qui est precedement crée 
-
+  afficheproduct;
 
 
 // crée une fonction qui prends en paramettre un object produit et qui renvoie le meme objet apres avoir multiplié son prix par deux
 // faire appel à cette fonction et affiche le resultat
+function multipleprice(prod1){
+       let newprice=(prod1.prix)*2;
+       return newprice;
+
+}
+        var reponse=multipleprice(prod1);
+
+
